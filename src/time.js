47dff8a9af3967265c78bd35f8c881905a4ff4bf
Simon Petrac <pfoerdie@gmail.com>
2023-01-23 16:49:09 +0100

    initial commit

---
+++ src/time.js
@@ -0,0 +1,8 @@
+const
+    util = require('./util.js'),
+    time = {};
+
+// TODO
+
+util.sealModule(time);
+module.exports = time;
---
+++ test/time.test.js
@@ -0,0 +1,12 @@
+const
+    expect = require('expect'),
+    {describe, test} = require('mocha'),
+    time = require('../src/time.js');
+
+describe('fua.core.time', function () {
+
+    test.only('develop', function () {
+        console.log(time);
+    });
+
+});
